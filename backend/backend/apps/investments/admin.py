b212669e9c283cc01d24cc44e4a74e11fb41eacb
abdujalalov <abdujalolovhusrav@gmail.com>
2024-09-06 22:57:45 +0500

    delete extra files

--- backend/backend/apps/investments/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-
-from apps.investments.models import Investment, Transaction, Payment
-
-admin.site.register(Investment)
-admin.site.register(Transaction)
-admin.site.register(Payment)
